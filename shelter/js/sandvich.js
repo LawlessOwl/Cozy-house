4a5185918023a5e406e8652aaef8b829126fd4b3
lawlessowl <erokhin-maksim@bk.ru>
2023-08-06 20:20:35 +0300

    fix: remove scroll lock body

--- shelter/js/sandvich.js
+++ shelter/js/sandvich.js
@@ -2,11 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
     let sandvichCheck = document.querySelector('.sandvich-menu')
     let sandvichButton = document.querySelector('.label-sandvich')
     let pageBody = document.querySelector('.main-body')
-        sandvichButton.addEventListener('click', function() {
-            if(sandvichCheck.checked) {
-                pageBody.classList.remove('lock-scroll')
-            } else {
-                pageBody.classList.add('lock-scroll')
-            }
-        })
+    let linkList = document.querySelectorAll('a[data-link="nav-item"]')
+
+    sandvichButton.addEventListener('click', function() {
+        if(sandvichCheck.checked) {
+            pageBody.classList.remove('lock-scroll')
+        } else {
+            pageBody.classList.add('lock-scroll')
+        }
+    })
+    
+    linkList.forEach((link) => link.addEventListener('click', function() {
+        pageBody.classList.remove('lock-scroll')
+        sandvichCheck.checked = false
+    }))
 })
